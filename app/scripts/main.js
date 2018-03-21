603338aeb12d7015042253d2ab6704f8407ccf06
SukachevAlex <sukachevalexandr@gmail.com>
2018-03-21 22:07:05 +0300

    make section guestbook, make adaptive section guestbook

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,24 @@ $(document).ready(function() {
         $('.nav').toggleClass('visible');
     });
 
+    $(".guestbook-slider-js").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        arrows : false,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: false
+                }
+            }
+        ]
+    });
+
 });
