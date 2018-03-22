81012d00c1ea2ffde9927822d4c5ce203e0eb03d
SukachevAlex <sukachevalexandr@gmail.com>
2018-03-22 03:11:58 +0300

    make footer, make footer adaptive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,5 +25,27 @@ $(document).ready(function() {
         ]
     });
 
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [55.751574, 37.573856],
+                zoom: 7,
+                type: 'yandex#hybrid',
+                controls: []
+            }, {
+                suppressMapOpenBlock: true
+            }),
+
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {})
+
+        myMap.geoObjects
+            .add(myPlacemark);
+    });
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
 
 });
